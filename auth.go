5a61cc000258c60b017130e213fdfa29d108147f
Mayo <mayo@linux.com>
2024-09-15 20:57:43 +0900

    Refactor authentication code and update module name

--- auth.go
+++ auth.go
@@ -1,3 +1,32 @@
 package vrchat
 
-import ()
+import (
+	"fmt"
+)
+
+// Authenticate authenticates the client with the VRChat API
+func (c *Client) Authenticate(username, password, totp string) error {
+	c.client.SetHeaders(map[string]string{
+		"User-Agent":   "vrchat-go/0.0.0 mayo@linux.com",
+		"Accept":       "application/json",
+		"Content-Type": "application/json",
+	})
+	resp, err := c.client.R().
+		SetBasicAuth(username, password).
+		SetBody(map[string]string{
+			"code": totp,
+		}).
+		Post("/auth/twofactorauth/totp/verify")
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode() != 200 {
+		return fmt.Errorf("failed to authenticate: %s", resp.String())
+	}
+
+	cookies := resp.Cookies()
+	c.client.SetCookies(cookies)
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/mayocream/vrchat
+module github.com/mayocream/vrchat-go
 
 go 1.23.1
 
